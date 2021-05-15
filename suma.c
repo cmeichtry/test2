7c5efd8ed0849d76f583f78e5c54fb7acaf0869a
cmeichtry <cmeichtry@itba.edu.ar>
2021-05-15 13:01:42 -0300

    Version 2

--- suma.c
+++ suma.c
@@ -8,15 +8,15 @@ int main(void){
 
     return 0;
 }  
-/*funcion original*/
+/*funcion original
 int suma_digitos(int n1){
     return ((n1 % 10) + digitos(n1 / 10)); //caso recursivo
 }
-/*
+
 suma lo digitos del entero que paso por argumento
 no funciona porque falta el caso base y cuando ocurre el caso recursivo lo llama con un nombre que no es el de la funcion
 */
-/*
+
 int suma_digitos(int n1){
     if(n1<1){
         return 0;
@@ -25,7 +25,7 @@ int suma_digitos(int n1){
         return ((n1 % 10) + suma_digitos(n1 / 10));
     }
 }
-*/
+
 /*Para subirlo a github
 Crear un repositorio de GitHub y subir un main utilizando la
 función tal cual está escrita. Luego, corregir la función,
@@ -38,7 +38,7 @@ Para subir el archivo la primera vez:
 3) cuando guardemos o creemos algun archivo en esa area. Hay que escribir git add suma.c,
    con esto nuestro archivo pasara a una "staging area".
 4) luego con el comando git commit el archivo pasara a un "local repository". Es opcional 
-   utilizar el flag -m para poner un mensaje para la version
+   utilizar el flag -m "Mensaje" para poner un mensaje para la version
 5) finalmente para subirlo a nuestro GitHub habra que usar el comando git push, luego de esto
    se nos pedira user y password, al introducirlas nuestro archivo quedara guardado en un 
    "remote repository"
